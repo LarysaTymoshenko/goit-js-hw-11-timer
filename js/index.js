a3bf4a2387abb92f07b24cf76c18f9bea30b6d7c
LarysaTymoshenko <luda76@ukr.net>
2021-10-12 00:07:18 +0300

    end timer

--- js/index.js
+++ js/index.js
@@ -7,26 +7,39 @@ const { daysRef, hoursRef, minsRef, secsRef } = refs;
 class CountdownTimer{
     constructor({ selector, targetDate }) {
         this.targetDate = targetDate;
-
         this.intervalId = null;
         this.deltaTime = 0;
         this.selector = selector;
+       
     }
     start() {
         this.intervalId = setInterval(() => {
-            let currentTime = Date.now()
-            this.deltaTime =  this.targetDate - currentTime;
-            console.log(this.deltaTime);
+            let currentTime = Date.now() // текущее время ежесекундно
+            this.deltaTime =  this.targetDate - currentTime; // разнца времени
+            const days = this.pad(Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)))
+            const hours= this.pad(Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+            const mins = this.pad(Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)))
+            const secs = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000))
+            this.countDown({ days, hours, mins, secs });
         }, 1000);
     }
     stop() {
         clearInterval(this.intervalId);
     }
 
+    
+    pad(value) {
+        return String(value).padStart(2, "0");
+    }
+    countDown({ days, hours, mins, secs }) {
+    daysRef.textContent = days;
+    hoursRef.textContent = hours;
+    minsRef.textContent = mins;
+    secsRef.textContent = secs;
+    };
+
 }
 
-// const myTimer = new CountdownTimer(new Date(2022, 0, 1))
-//  const myTimer =  new CountdownTimer(targetDate)
    const myTimer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jan 01, 2022'),
@@ -35,4 +48,4 @@ class CountdownTimer{
 
 console.log(myTimer)
 myTimer.start()
-myTimer.stop()
+// myTimer.stop()
